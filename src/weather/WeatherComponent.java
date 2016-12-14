f4210c95ebcd620f3a2e56dbf9afc98e638f2bfb
thomas <thomas@thomas-X555LJ>
2016-12-14 14:08:49 +0100

    2nd Commit

--- src/weather/WeatherComponent.java
+++ src/weather/WeatherComponent.java
@@ -7,6 +7,7 @@ package weather;
 
 import java.awt.Canvas;
 import java.awt.Graphics;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -14,12 +15,103 @@ import java.util.Iterator;
  *
  * @author Thomas Lecointre
  */
-class WeatherComponent extends Canvas implements Runnable {
+class WeatherComponent extends Canvas implements Runnable, Serializable {
 
     private final long second = 1000;
     private final long refreshDelay = 60 * second; // 1 minute
     private boolean alive = true;
-    private String apiKey = "3670017b342a20200b54ab598cc868d0";
+    private final String apiKey = "3670017b342a20200b54ab598cc868d0";
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(float minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public float getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(float maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+    }
+
+    public float getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(float windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public int getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(int windDirection) {
+        this.windDirection = windDirection;
+    }
+    
+    private String city;
+    private float temperature;
+    private float minTemperature;
+    private float maxTemperature;
+    private int pressure;
+    private int humidity;
+    private int visibility;
+    private float windSpeed;
+    private int windDirection;
+    
     
     private final ArrayList<RefreshListener> listeners = new ArrayList<>();
     
@@ -27,22 +119,21 @@ class WeatherComponent extends Canvas implements Runnable {
         return this.apiKey;
     }
     
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
-    }
-    
     public WeatherComponent() { 
         super.setSize(640, 480); 
     }
+    
     
     public void start() {
         new Thread(this).start(); 
     }
     
+    
     public void stop() {
         alive = false;
     }
     
+    @Override
     public void run() {
         while (alive) {
             try {
@@ -62,8 +153,9 @@ class WeatherComponent extends Canvas implements Runnable {
         listeners.remove(r);
     }
     
+    @Override
     public void paint(Graphics g) {
-        g.drawString("Timer", 0, 15);
+        g.drawString("Test", 0, 15);
     }
 
     private void fireRefreshEvent(RefreshEvent refreshEvent) {
